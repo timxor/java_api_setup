33d6d04e277fe953270b34717015d91a81bbdd1a
Tim Siwula <siwulactim@gmail.com>
2025-01-05 08:33:47 -0600

    add logging

--- src/main/java/net/siwula/api/ApiController.java
+++ src/main/java/net/siwula/api/ApiController.java
@@ -1,8 +1,9 @@
 package net.siwula.api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/api/v1/employees")
 public class ApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
+
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        logger.info("Returning response: 'Hello " + name + "'");
+
         return String.format("Hello, %s!", name);
     }
 }
