faa18fa3e5b90e444af208855576d7ac439fbbb8
Tim Siwula <siwulactim@gmail.com>
2026-01-30 17:46:26 -0600

    Only open swagger docs in browser when running the app

--- src/main/java/net/siwula/api/ApiApplication.java
+++ src/main/java/net/siwula/api/ApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -14,24 +15,24 @@ public class ApiApplication {
     }
 
     @Bean
+    @Profile("local")
     public ApplicationRunner openBrowser() {
         return args -> {
             System.out.println("Attempting to open browser...");
             String url = "http://localhost:8080/swagger-ui/index.html";
             String os = System.getProperty("os.name").toLowerCase();
             Runtime rt = Runtime.getRuntime();
+
             try {
                 if (os.contains("mac")) {
+                    System.out.println("Detected macOS. Launching swagger docs in browser.");
                     rt.exec("open " + url);
-                } else if (os.contains("win")) {
-                    rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
-                } else if (os.contains("nix") || os.contains("nux")) {
-                    rt.exec("xdg-open " + url);
+                    return;
                 } else {
-                    System.out.println("OS not supported for auto-open.");
+                    System.out.println("OS not supported for launching swagger docs in browser.");
                 }
             } catch (IOException e) {
-                System.out.println("Failed to open browser: " + e.getMessage());
+                System.out.println("Failed to launch swagger docs browser: " + e.getMessage());
             }
         };
     }
